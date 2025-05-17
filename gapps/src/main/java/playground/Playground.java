dd1c76acb7d331c3055931e627172aada8996929
AleksandraZecevic <152481072+bunnyAKi@users.noreply.github.com>
2025-05-17 13:00:44 +0200

    FORTH - reading from data, correct file paths

--- gapps/src/main/java/playground/Playground.java
+++ gapps/src/main/java/playground/Playground.java
@@ -1,5 +1,20 @@
 package playground;
 
-public class Playground {
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Playground {
+    public static void main(String[] args) throws IOException {
+
+        String soilFile = "dataSets/otawwa_ks_soil.csv";
+        String weatherFile = "dataSets/ottawa_ks_weather.csv";
+
+        SoilData soil = SoilDataLoader.loadSoilData(soilFile);
+        ArrayList<WeatherDay> weatherList = (ArrayList<WeatherDay>) WeatherDataLoader.loadWeatherData(weatherFile);
+
+        // May 18 to October 2 in span of 9 years
+        for(int i = 0; i < weatherList.size(); i++) {
+            System.out.print(weatherList.get(i));
+        }
+    }
 }
